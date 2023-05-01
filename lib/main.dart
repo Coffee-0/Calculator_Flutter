00897ea5f553aa70763924f58db41378eddbcb63
Hemanth <coffee-0@Hemanths-MacBook-Air.local>
2023-05-01 11:32:39 +0530

    finished building the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:calculator/buttons.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,6 +23,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'X',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'Ans',
+    '='
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,18 +55,48 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             child: Container(),
           ),
-          const Expanded(
+          Expanded(
             flex: 2, // Make this twice as large as the top side
-            child: Center(
-              child: MyButton(
-                color: Colors.deepPurple,
-                textColor: Colors.white,
-                buttonText: '0',
-              ),
-            ),
+            child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: Colors.green,
+                      textColor: Colors.white,
+                    );
+                  } else if (index == 1) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: Colors.red,
+                      textColor: Colors.white,
+                    );
+                  } else {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? Colors.deepPurple
+                          : Colors.deepPurple[50],
+                      textColor: isOperator(buttons[index])
+                          ? Colors.white
+                          : Colors.deepPurple,
+                    );
+                  }
+                }),
           ),
         ],
       ),
     );
+  }
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'X' || x == '+' || x == '-' || x == '=') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
